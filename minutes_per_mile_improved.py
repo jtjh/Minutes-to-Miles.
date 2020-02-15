b902041344c6f3f896695ac532d3be518dfa1c8b
Joshua Hardy <joshuahardy95@gmail.com>
2020-02-16 01:21:58 +0800

    even better

--- minutes_per_mile_improved.py
+++ minutes_per_mile_improved.py
@@ -1,55 +1,41 @@
 def kms_ran_input(message):
     while True:
         try:
-            kms_ran = float(input(message))
+            get_kms_ran = float(input(message))
         except ValueError:
-            print(
-                "Sorry, Could you please enter the number of kilometres you ran only. "
-            )
+            print("Sorry, Could you please enter Numerical digits only. ")
+            continue
+        if get_kms_ran <= 0:
+            print("Sorry, Can you please Enter a Positive number greater then 0. ")
             continue
         else:
-            return kms_ran
+            return get_kms_ran
 
 
 def minutes_ran_input(message):
     while True:
         try:
-            minutes_ran = float(input(message))
+            get_minutes_ran = float(input(message))
         except ValueError:
-            print("Sorry, Could you please enter the number of minutes you ran only. ")
+            print("Sorry, Could you please enter Numerical digits only. ")
+            continue
+        if get_minutes_ran <= 0:
+            print("Sorry, Can you please Enter a Positive number greater then 0. ")
             continue
         else:
-            return minutes_ran
-
-
-def minutes_per_km_0():
-    while True:
-        try:
-            minutes_per_km_0 = minutes_ran / kms_ran
-        except ZeroDivisionError:
-            print("Thats Illegal! Im calling the Police! ")
-            return exit()
-        else:
-            return minutes_per_km_0
+            return get_minutes_ran
 
 
 kms_ran = kms_ran_input("How many kilometres did you run? ")
 minutes_ran = minutes_ran_input("how many Minutes did you run for? ")
-
-
+minutes_per_km = minutes_ran / kms_ran
 miles_to_kms = 1.61
-seconds_per_minute = 60
-milliseconds_per_second = 100
-minutes_per_km = minutes_per_km_0()
 minutes_per_mile = minutes_per_km * miles_to_kms
 minutes_integer = int(minutes_per_mile)
 seconds_float = minutes_per_mile - minutes_integer
+seconds_per_minute = 60
 seconds_multiplied = seconds_float * seconds_per_minute
 seconds_integer = int(seconds_multiplied)
-milliseconds_float = seconds_multiplied - seconds_integer
-milliseconds_mutiplied = milliseconds_float * milliseconds_per_second
-milliseconds_integer = int(milliseconds_mutiplied)
-
 
 print(
     "You ran an average of",
@@ -57,8 +43,5 @@ print(
     "minutes",
     seconds_integer,
     "seconds",
-    #    "and",
-    #    milliseconds_integer,
-    #    "milliseconds",
     "per mile.",
 )
